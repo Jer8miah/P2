6694caba77180a4faf7321f800d21aad23e4506b
Jeremiah Douglas <jeremiahd1018@gmail.com>
2024-01-21 20:07:58 -0600

    first commit

---
+++ config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+    pageTitle: "Crushifier",
+    popupTitle: '😏😏😏',
+    description: `I'm assuming you're here because you've been sent this by me`,
+    introButton: 'I want to ask you something...',
+    title: 'Will you be my valentine?',
+    yesBtn: 'Yess 🥺',
+    noBtn: 'No :3',
+    question: 'Glad to know you feel the same way 😘😘 \n Now.. tell me why you love me ?',
+    sendBtn: 'Send',
+    reply: 'Cuz what\'s there to not love about you? You\'re so cute, funny, and smart. You\'re the best thing that\'s ever happened to me. I love you so much 😘😘😘',
+    messPop: 'I know. I\'m just that good 😎😎😎',
+    messDesc: 'Now you\'re my valentine 👉👈',
+    btnAccept: 'Okiiiiieeee <3',
+    // make it wait a few seconds before redirecting
+    
+    redirect: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+}

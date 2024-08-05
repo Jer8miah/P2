947a4c43211d357bce1870def202be391f593e17
Jeremiah Douglas <jeremiahd1018@gmail.com>
2024-08-04 21:59:39 -0500

    update

--- config.js
+++ config.js
@@ -3,16 +3,16 @@ const CONFIG = {
     popupTitle: '😏😏😏',
     description: `I'm assuming you're here because you've been sent this by me`,
     introButton: 'I want to ask you something...',
-    title: 'Will you be my valentine?',
+    title: 'Will you be my girlfriend?',
     yesBtn: 'Yess 🥺',
     noBtn: 'No :3',
     question: 'Glad to know you feel the same way 😘😘 \n Now.. tell me why you love me ?',
     sendBtn: 'Send',
     reply: 'Cuz what\'s there to not love about you? You\'re so cute, funny, and smart. You\'re the best thing that\'s ever happened to me. I love you so much 😘😘😘',
     messPop: 'I know. I\'m just that good 😎😎😎',
-    messDesc: 'Now you\'re my valentine 👉👈',
+    messDesc: 'Now you\'re my girlfriend 👉👈',
     btnAccept: 'Okiiiiieeee <3',
     // make it wait a few seconds before redirecting
     
-    redirect: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+    redirect: 'https://youtu.be/Aki05PCJWtE?si=AWbPd4O4XbcWjyP8',
 }
